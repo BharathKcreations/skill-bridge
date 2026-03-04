1c063ce11b238a7c941b9c83cababa36492a68ab
Bharath k <bk1828330@gmail.com>
2026-03-04 23:04:19 +0530

    fix render open

--- app.py
+++ app.py
@@ -160,5 +160,8 @@ def connect():
         "connection_success.html",
         mentor=selected_mentor
     )
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
