f09a67508685aeb387def89062215ed55737e060
Bharath k <bk1828330@gmail.com>
2026-04-05 21:53:12 +0530

    fixed app run for deployment

--- app.py
+++ app.py
@@ -403,7 +403,7 @@ def register_peer():
 })
     return redirect(url_for("mentors"))
     
-# ---------------- RUN APP ----------------
+# ----------- RUN APP -----------
+
 if __name__ == "__main__":
- port = int(os.environ.get("PORT", 10000))
-app.run(host="0.0.0.0", port=port, debug=True)
+    app.run(debug=True)
